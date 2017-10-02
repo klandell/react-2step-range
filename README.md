381b443b602dc50737e14b30d6c0cd71756c84f3
Kyle Landell <kyle.landell@gmail.com>
2017-10-01 22:31:34 -0400

    Prepare for initial npm publish

---
+++ README.md
@@ -0,0 +1,1 @@
+If you happen to somehow find this package, please don't use it yet... It is still in development.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { default as CoarseIncrement } from './CoarseIncrement';
+export { default as FineIncrement } from './FineIncrement';
+export { default as MinusIcon } from './MinusIcon';
+export { default as PlusIcon } from './PlusIcon';
+export { default as Slider } from './Slider';
+export { default as Ticks } from './Ticks';
